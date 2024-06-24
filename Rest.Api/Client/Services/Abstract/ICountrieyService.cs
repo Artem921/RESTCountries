4bb71c5636a5cd2280c719c4b5ab32f5c892fd3d
Игнат Игнат <Genezis2022@outlook.com>
2024-06-24 10:42:53 +0300

    рефакторинг

--- Rest.Api/Client/Services/Abstract/ICountrieyService.cs
+++ Rest.Api/Client/Services/Abstract/ICountrieyService.cs
@@ -5,7 +5,6 @@ namespace Client.Services.Interfaces
     public interface ICountrieyService
     {
         public Task<IEnumerable<Country>> GetAllCounties();
-
         public Task<List<Country>> GetCountryByName(string name);
         public Task<List<Country>> GetCountryByFullName(string name,bool fullText);
     }
